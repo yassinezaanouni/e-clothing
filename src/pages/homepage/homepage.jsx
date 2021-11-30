8fe549ded7085b136671cda0f58c5fdf78aa7e7a
Yassine Zaaanouni <yassinezaanouni@gmail.com>
2021-11-30 13:54:13 +0100

    added directory, menu-item into home page and their style

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.scss";
+import Direcroty from "../../components/directory/directory";
+const HomePage = () => (
+  <div className="homepage">
+    <Direcroty />
+  </div>
+);
+export default HomePage;
